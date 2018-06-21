ae79fca7716f5dbdacf4102ad6ded5dfbdbf947e
Franklin Antony <franklinanton.007@gmail.com>
2018-06-21 14:49:59 +0530

    Added new fscript file

---
+++ script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC-pMGLqJZUEvexMM2pFiIuyGW4zbjH6Kk",
+  authDomain: "lavanya-beauty-clinic-website.firebaseapp.com",
+  databaseURL: "https://lavanya-beauty-clinic-website.firebaseio.com",
+  projectId: "lavanya-beauty-clinic-website",
+  storageBucket: "lavanya-beauty-clinic-website.appspot.com",
+  messagingSenderId: "613959501953"
+};
+firebase.initializeApp(config);
